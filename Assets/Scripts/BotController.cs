7933883c8810ff29d30e822f0c724199ab318d1f
celadia <celadia@21-school.ru>
2022-08-28 03:20:53 +0300

    Add Bot Script

--- Assets/Scripts/BotController.cs
+++ Assets/Scripts/BotController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,31 +7,37 @@ using UnityEngine.AI;
 public class BotController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> pathArray;
+    [SerializeField] private float precision = 0.2f;
 
     private NavMeshAgent _agent;
+    private int _i = 0;
     
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.SetDestination(pathArray[_i].transform.position);
     }
 
     // Update is called once per frame
     private void LateUpdate()
     {
-        // Not work!!!
-        // PathGeneration();
+        if (Math.Abs(transform.position.x - pathArray[_i].transform.position.x) < precision && Math.Abs(transform.position.z - pathArray[_i].transform.position.z) < precision)
+        {
+            if (++_i >= pathArray.Count)
+            {
+                _i = 0;
+            }
+            
+            PathGeneration(pathArray[_i].transform.position);
+        }
     }
 
-    private void PathGeneration()
+    private void PathGeneration(Vector3 point)
     {
-        foreach (var point in pathArray)
-        {
-            while (transform.position != point.transform.position && _agent.SetDestination(point.transform.position))
-            {
-                _agent.SetDestination(point.transform.position);
-            }
-        }
-        Debug.Log("Next Point");
+        _agent.SetDestination(point);
+        
+        if (Debug.isDebugBuild)
+            Debug.Log("Next Point: " + point);
     }
 }
