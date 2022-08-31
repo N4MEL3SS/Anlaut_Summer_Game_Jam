a7591ccc477936e1910f90068c11fea780b09051
Reelydoit Toast <rtoast@pr-g3.kzn.21-school.ru>
2022-08-31 03:14:02 +0300

    menu

--- Assets/masha/TestSfer.cs
+++ Assets/masha/TestSfer.cs
@@ -1,24 +1,55 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestSfer 
     : MonoBehaviour
 {
     public GameObject enemy;
-    // public GameObject Texts;
+    public GameObject dot;
+    public GameObject menuTwo;
+    public GameObject menuOne;
   void OnMouseDown()
   {
       if (PlayerPrefs.HasKey("Cub"))
       {
-          Instantiate(enemy);
-          // PlayerPrefs.DeleteKey("Cube");
+          // Instantiate(enemy);
+          PlayerPrefs.DeleteKey("Cub");
+          dot.SetActive(false);
+          menuOne.SetActive(true);
+          menuTwo.SetActive(true);
       }
+      
   }
+
+  public void One()
+  {
+      Instantiate(enemy);
+      menuTwo.SetActive(false);
+      menuOne.SetActive(false);
+  }
+
+  public void two()
+  {
+      menuTwo.SetActive(false);
+      menuOne.SetActive(false);
+  }
+  
   
   // void OnMouseOver()
   // {
-  //     Text.SetActive(true);
+  //     if (PlayerPrefs.HasKey("Cub"))
+  //     {
+  //         dot.SetActive(false);
+  //     }
+  //     // Texts.SetActive(true);
   // }
+
+  private void OnMouseExit()
+  {
+      // Texts.SetActive(false);
+  }
 }
